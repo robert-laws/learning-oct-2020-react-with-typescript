f58fafd2de7de19d00cdd33a38e64e3e22e6d9a2
Robert Laws <roblaws@me.com>
2020-10-16 12:42:39 +0300

    add basic todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+  const todos = [{id: 't1', text: 'go to the grocery store'}]
   return (
     <div className='App'>
-      <h2>App</h2>      
+      <h2>App</h2>   
+      <TodoList items={todos} />   
     </div>
   );
 }
